2c884c50bdbb26be7be34e09c78f9a681736e199
unknown <mauroariel.sosa031@gmail.com>
2022-10-20 23:10:28 -0500

    Added: Get all pods route formated

--- backend/src/routes/pods.ts
+++ backend/src/routes/pods.ts
@@ -2,7 +2,19 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { CMD } from "../utils/cmd";
 
 async function getPods(req: FastifyRequest, res: FastifyReply) {
-    return await CMD.exec('kubectl get pods -A');
+    const pods = await CMD.exec('kubectl get pods -A');
+
+    return pods.split('\n').map((line) => {
+        const row = line.split(' ').filter((field) => field != ' ');
+        return {
+            namespace: row[0],
+            name: row[1],
+            containers_count: row[2],
+            state: row[3],
+            restarts: row[4],
+            created_at: row[5]
+        }
+    });
 }
 
 
