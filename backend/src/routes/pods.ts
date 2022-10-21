b58078b17f6223827eb3f637a3ba85458f8d4275
unknown <mauroariel.sosa031@gmail.com>
2022-10-20 23:31:08 -0500

    Added: Get all pods route, added content type

--- backend/src/routes/pods.ts
+++ backend/src/routes/pods.ts
@@ -2,22 +2,28 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { CMD } from "../utils/cmd";
 
 async function getPods(req: FastifyRequest, res: FastifyReply) {
-    const pods = await CMD.exec('kubectl get pods -A');
+        res.header("Content-Type", "application/json; charset=utf-8");
 
-    const jsonPods = pods.split('\n').map((line) => {
-        const row = line.split(' ').filter((field) => field != ' ' && field != '');
-        const jsonRow = {
-            namespace: row[0],
-            name: row[1],
-            containers_count: row[2],
-            state: row[3],
-            restarts: row[4],
-            created_at: row.splice(5, row.length).join(' '),
-        }
-        return jsonRow;
+    CMD.exec('kubectl get pods -A').then((pods) => {
+        const jsonPods = pods.split('\n').map((line) => {
+            const row = line.split(' ').filter((field) => field != ' ' && field != '');
+            const jsonRow = {
+                namespace: row[0],
+                name: row[1],
+                containers_count: row[2],
+                state: row[3],
+                restarts: row[4],
+                created_at: row.splice(5, row.length).join(' '),
+            }
+            return jsonRow;
+        });
+        res.code(200);
+        res.send(jsonPods.splice(0, jsonPods.length - 1));
+
+    }).catch((err) => {
+        res.code(500);
+        res.send({ message: err });
     });
-    console.log(jsonPods);
-    return jsonPods.splice(0, jsonPods.length - 1);
 }
 
 
