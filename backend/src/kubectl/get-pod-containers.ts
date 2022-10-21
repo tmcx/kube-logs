02f2e42ab2eed72a2657cd0bb7a37cac918a6e79
TMCX <mauroariel.sosa031@gmail.com>
2022-10-21 17:34:41 -0500

    log get pod containers

--- backend/src/kubectl/get-pod-containers.ts
+++ backend/src/kubectl/get-pod-containers.ts
@@ -5,7 +5,8 @@ export async function getPodContainers(podName: string): Promise<string[]> {
     try {
         const namespace = await getPodNamespace(podName);
 
-        let cmd = `kubectl get pod/${podName} -o jsonpath='{.spec.containers[*].name}' -n ${namespace}`
+        let cmd = `kubectl get pod/${podName} -o jsonpath='{.spec.containers[*].name}' -n ${namespace}`;
+        console.log(cmd);
         const containers = await CMD.exec(cmd);
 
         return containers.split(' ');
